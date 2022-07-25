27a37d3e9b446b77c29fdc514ba69ce09a362c3c
Leo Kang <93236227+a196xl3gj3@users.noreply.github.com>
2022-07-25 20:48:30 +0800

    Update Work220724_2.java

--- 0724/Work220724_2.java
+++ 0724/Work220724_2.java
@@ -1,14 +1,13 @@
-
 public class Work220724_2 {
 	// 定義一個方法
-	static void bubble(int[] arr) {
+	static void bubblesmall(int[] arr) {
 		// 預設 n 為 陣列 arr 的長度
 		int x = arr.length;
 		int temp = 0;
 		for(int i=0 ; i<x ; i++) {
 			// 比較後就往下走
 			for(int j=1 ; j<=(x-1) ; j++) {
-				// 比較左右邊陣列
+				// 比較左右邊陣列，當比較值大於原值就交換位置
 				if(arr[j-1] > arr[j]) {
 				// 比較後交換元素
 					temp = arr[j-1];
@@ -18,6 +17,25 @@ public class Work220724_2 {
 			}
 		}
 	}
+	// 定義一個方法
+	static void bubblebig(int[] arr) {
+		// 預設 n 為 陣列 arr 的長度
+		int x = arr.length;
+		int temp = 0;
+		for(int i=0 ; i<x ; i++) {
+			// 比較後就往下走
+			for(int j=1 ; j<=(x-1) ; j++) {
+				// 比較左右邊陣列，當比較值小於原值就交換位置
+				if(arr[j-1] < arr[j]) {
+				// 比較後交換元素
+					temp = arr[j-1];
+					arr[j-1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		int arr[] = {40, 80, 50, 10, 70, 100, 90, 20, 60, 30};
 		
@@ -27,9 +45,9 @@ public class Work220724_2 {
 		}
 		System.out.println();
 		// 開始泡沫排序，帶入陣列參數
-		bubble(arr);
+		bubblesmall(arr);
 		// 排序後呈現
-		System.out.println("使用泡沫排序");
+		System.out.println("使用泡沫排序(由小到大)");
 //		for(int i=0 ; i< arr.length; i++) {
 //			System.out.print(arr[i]+" ");
 //		}
@@ -38,6 +56,16 @@ public class Work220724_2 {
 			System.out.print(a + " ");
 		}
 		System.out.println();
+		// 開始泡沫排序，帶入陣列參數
+		bubblebig(arr);
+		// 排序後呈現
+		System.out.println("使用泡沫排序(由大到小)");
+		// 變數的型態是甚麼，就必須要甚麼來做
+		for(int b:arr) { //foreach 從陣列 0~9 結束就離開
+			System.out.print(b + " ");
+		}
+		System.out.println();
+		
 		
 	}
 }
